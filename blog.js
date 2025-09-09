6019b512fe844ca6de0b0ba6f2c14074ba0287d6
EmanueleIppolito <emanuele2410@gmail.com>
2025-09-09 19:33:01 +0200

    router

--- blog.js
+++ blog.js
@@ -1,92 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const postsRouter = require('./routes/posts')
 
 app.use(express.static('public'))
 
+
+
+//Routing
+
+app.use('/posts', postsRouter)
+
+
+
 app.listen(port, () =>{
     console.log(`State guardando il mio blog sulla porta ${port}`)
-})
-
-
-const post =[
-    {
-        id: 1,
-        titolo: "Ciambellone",
-        contenuto: "Ricetta Ciambellone",
-        foto: "/images/ciambellone.jpeg",
-        tag: ["dolce", "veloce"]
-    },
-    {
-        id: 2,
-        titolo: "Cracker di Barbabietola",
-        contenuto: "Ricetta Cracker di barbabietola",
-        foto: "/images/cracker_barbabietola.jpeg",
-        tag: ["salato", "healty"]
-    },
-    {
-        id: 3,
-        titolo: "Pane fritto dolce",
-        contenuto: "Ricetta pane fritto dolce",
-        foto: "/images/pane_fritto_dolce.jpeg",
-        tag: ["dolce", "fritto"]
-    },
-    {
-        id: 4,
-        titolo: "Pasta di barbabietola",
-        contenuto: "Ricetta pasta di barbabietola",
-        foto: "/images/pasta_barbabietola.jpeg",
-        tag: ["salato", "fit"]
-    },
-    {
-        id: 5,
-        titolo: "Torta Paesana",
-        contenuto: "Ricetta torta paesana",
-        foto: "/images/torta_paesana.jpeg",
-        tag: ["dolce", "feste"]
-    }
-]
-
-
-//Routes
-
-app.get('/', (req, res) =>{
-    res.send('<h1>Server del mio blog</h1>')
-})
-
-
-//CRUD Operations
-// Index
-app.get('/api/posts', (req, res) =>{
-    res.json({post})
-})
-
-// Show
-
-app.get('/api/posts/:id', (req, res) =>{
-    res.send('Show the single post with ID' + req.params.id)
-})
-
-//Store
-
-app.post('/api/posts/', (req, res) => {
-    res.send('Create a new post')
-})
-
-// Update
-
-app.put('/api/posts/:id', (req, res) => {
-    res.send('Update the entire post with ID' + req.params.id)
-})
-
-//Modify
-
-app.patch('/api/posts/:id', (req, res) => {
-    res.send('Partial update for a single post with ID' + req.params.id)
-})
-
-//Destroy
-
-app.delete('/api/posts/:id', (req, res) =>{
-    res.send('Delete the single post with ID' + req.params.id)
 })
---
+++ routes/posts.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const router = express.Router();
+
+const post =[
+    {
+        id: 1,
+        titolo: "Ciambellone",
+        contenuto: "Ricetta Ciambellone",
+        foto: "/images/ciambellone.jpeg",
+        tag: ["dolce", "veloce"]
+    },
+    {
+        id: 2,
+        titolo: "Cracker di Barbabietola",
+        contenuto: "Ricetta Cracker di barbabietola",
+        foto: "/images/cracker_barbabietola.jpeg",
+        tag: ["salato", "healty"]
+    },
+    {
+        id: 3,
+        titolo: "Pane fritto dolce",
+        contenuto: "Ricetta pane fritto dolce",
+        foto: "/images/pane_fritto_dolce.jpeg",
+        tag: ["dolce", "fritto"]
+    },
+    {
+        id: 4,
+        titolo: "Pasta di barbabietola",
+        contenuto: "Ricetta pasta di barbabietola",
+        foto: "/images/pasta_barbabietola.jpeg",
+        tag: ["salato", "fit"]
+    },
+    {
+        id: 5,
+        titolo: "Torta Paesana",
+        contenuto: "Ricetta torta paesana",
+        foto: "/images/torta_paesana.jpeg",
+        tag: ["dolce", "feste"]
+    }
+]
+
+//Routes
+
+router.get('/', (req, res) =>{
+    res.send('<h1>Server del mio blog</h1>')
+})
+
+
+//CRUD Operations
+// Index
+router.get('/', (req, res) =>{
+    res.json({post})
+})
+
+// Show
+
+router.get('/:id', (req, res) =>{
+    res.send('Show the single post with ID' + req.params.id)
+})
+
+//Store
+
+router.post('/', (req, res) => {
+    res.send('Create a new post')
+})
+
+// Update
+
+router.put('/:id', (req, res) => {
+    res.send('Update the entire post with ID' + req.params.id)
+})
+
+//Modify
+
+router.patch('/:id', (req, res) => {
+    res.send('Partial update for a single post with ID' + req.params.id)
+})
+
+//Destroy
+
+router.delete('/:id', (req, res) =>{
+    res.send('Delete the single post with ID' + req.params.id)
+})
+module.exports = router
